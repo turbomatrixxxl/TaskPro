f66b58792ab976d8f713a7936652dfa52514f3a7
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2025-01-16 11:53:42 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Button from "./components/commonComponents/Button";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button>Cucu</Button>
+    </div>
+  );
 }
 
 export default App;
