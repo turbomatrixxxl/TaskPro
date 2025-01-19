72f2f2649a51fb7fbd94d6dc3c267ea9f445609c
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2025-01-20 00:23:42 +0200

    first try

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import "./App.css";
+// import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+// function App() {
+//   return <div className="App"></div>;
+// }
 
-export default App;
+// export default App;
 
-
-/*import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Welcome from './components/Welcome/Welcome'; 
-import './styles/variables.css';  
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Welcome from "./components/Welcome/Welcome";
+import "./styles/variables.css";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/TaskPro" element={<Welcome />} />
-        
       </Routes>
     </Router>
   );
 }
 
-export default App;*/
+export default App;
