085bb00352366c18a66467b30aadcb7cba208d2d
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2025-01-29 09:23:27 +0200

    experiment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import "./styles/variables.css";
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store";
+// import { BrowserRouter } from "react-router-dom";
+
+// // Correct basename for GitHub Pages
+// const basename = process.env.NODE_ENV === "production" ? "/TaskPro" : "/";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <BrowserRouter
+//         future={{
+//           v7_startTransition: true, // Enable startTransition for state updates
+//           v7_relativeSplatPath: true, // Enable relative path handling for splats
+//         }}
+//         basename={basename}>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -6,23 +37,16 @@ import reportWebVitals from "./reportWebVitals";
 import "./styles/variables.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
-
-// Correct basename for GitHub Pages
-const basename = process.env.NODE_ENV === "production" ? "/TaskPro" : "/";
+import { HashRouter } from "react-router-dom";
+// import "normalize.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter
-        future={{
-          v7_startTransition: true, // Enable startTransition for state updates
-          v7_relativeSplatPath: true, // Enable relative path handling for splats
-        }}
-        basename={basename}>
+      <HashRouter basename="/">
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
